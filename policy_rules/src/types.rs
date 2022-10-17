dd652ec94886d8a523034d214bdac7a55e86d3db
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2022-10-17 12:52:01 +0400

    Add image url

--- policy_rules/src/types.rs
+++ policy_rules/src/types.rs
@@ -43,6 +43,7 @@ pub struct TokenMetadata {
     pub title: Option<String>, // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
     pub description: Option<String>, // free-form description
     pub media: Option<String>, // URL to associated media, preferably to decentralized, content-addressed storage
+    pub image_url: Option<String>, // URL to associated media image, preferably to decentralized, content-addressed storage
     pub media_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
     pub copies: Option<u64>, // number of copies of this set of metadata in existence when token was minted.
     pub issued_at: Option<u64>, // When token was issued or minted, Unix epoch in milliseconds
