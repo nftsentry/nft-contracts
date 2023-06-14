06d482baf9875e7b19c0abccc746b0f86f8af2cb
Chris Clason <clasoncj@Chriss-MacBook-Air.local>
2023-06-14 06:19:04 -0500

    Updating how percentage is calculated

--- nftsentry/src/mint.rs
+++ nftsentry/src/mint.rs
@@ -5,7 +5,7 @@ use common_types::types::{NFTMintResult};
 use common_types::utils::{balance_from_string, format_balance};
 use crate::*;
 
-const SLIPPAGE_PERCENTS: i32 = 3;
+const SLIPPAGE_PERCENTS: u128 = 3;
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
@@ -182,7 +182,8 @@ impl Contract {
             let mut price = balance_from_string(price_str.clone());
             if deposit < price && !opts.is_gift {
                 let reserved_price = deposit - balance_from_string("0.1".to_string());
-                let minimum_price = price * (100 - SLIPPAGE_PERCENTS) as u128;
+                let mut minimum_price = price * (100 - SLIPPAGE_PERCENTS) as u128;
+		minimum_price = minimum_price / 100 as u128;
                 if reserved_price < minimum_price {
                     return Err(format!(
                         "Attached deposit of {} NEAR is less than SKU price of {} NEAR (with {} slippage)",
